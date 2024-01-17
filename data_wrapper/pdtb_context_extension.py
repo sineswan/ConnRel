736a24723b7f3818ea23e3e0c96e67480b684acf
wan049 <Stephen.Wan@csiro.au>
2024-01-17 12:25:13 +0100

    DEBUG test... making the context preprocessing like the original no context case just to double check.

--- data_wrapper/pdtb_context_extension.py
+++ data_wrapper/pdtb_context_extension.py
@@ -387,7 +387,8 @@ def read_pdtb_sample(cur_samples, input_filename, raw_text_location, dataset="pd
                             new_arg1_string = re.sub(' +', ' ', new_merged_string)
 
                             #add delimiter before arg1
-                            new_arg1_string = new_arg1_string.replace(sample['arg1'], ' ... '+sample['arg1'])
+                            # new_arg1_string = new_arg1_string.replace(sample['arg1'], ' ... '+sample['arg1'])
+                            new_arg1_string = ' ... ' + sample['arg1']
 
                             # print(f"new arg1: {new_arg1_string}")
                             # print(f"-------------------------------------------")
