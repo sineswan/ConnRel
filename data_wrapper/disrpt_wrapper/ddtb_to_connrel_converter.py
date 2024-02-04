76cbecb00b6ad77b75d5e336c44d344e7728812f
wan049 <Stephen.Wan@csiro.au>
2024-02-04 15:42:53 +0100

    Adding 2 variations for Mode 1 data generation

--- data_wrapper/disrpt_wrapper/ddtb_to_connrel_converter.py
+++ data_wrapper/disrpt_wrapper/ddtb_to_connrel_converter.py
@@ -81,7 +81,7 @@ def convert(relation, context_index=None,  context_mode=None,
 
                 modified_arg1 = context + " ... " + arg1
                 if context_mode==1.1:    #use only context relationship
-                    modified_arg1 = context_rel_type + " ... " + arg1
+                    modified_arg1 = f"[{context_rel_type}]" + " ... " + arg1
                 elif context_mode==1.2:  #use both context string AND (prepended) context relationship
                     modified_arg1 = f"[{context_rel_type}] {modified_arg1}"
                 result["arg1"] = modified_arg1
